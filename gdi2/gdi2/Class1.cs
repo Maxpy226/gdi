d665fe50e8eccbb793710929a58d21609224f9af
Maxpy226 <db@servus.at>
2025-08-08 22:20:47 +0200

    wrwfwf

--- gdi2/gdi2/Class1.cs
+++ gdi2/gdi2/Class1.cs
@@ -741,6 +741,7 @@ namespace gdi2
                 BitBlt(hdc, randsec, r.Next(-4, 4), r.Next(100), y, hdc, randsec, 0, TernaryRasterOperations.SRCCOPY);
                 SelectObject(hdc, brush);
                 PatBlt(hdc, 0, 0, x, y, TernaryRasterOperations.PATINVERT);
+                DeleteObject(brush);
                 DeleteDC(hdc);
             }
 
